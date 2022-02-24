687cecce3f7597056e065d2678c7ba5583f466b0
TheBadams <bpeteradams@gmail.com>
2022-02-24 10:18:44 -0500

    HomePage(Content): Wrote some blurbs about the aims of the page and about how it has been written using Next.js and Typescript

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from 'next/head';
-import Link from 'next/link';
 import { NavBar } from "../components/navBar";
 
 const AboutMe: NextPage = () => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,12 @@ const Home: NextPage = () => {
 				<h1>Portfolio Page</h1>
 				<h2>The Personal Portfolio Website of Brian P. Adams</h2>
 				<text>This Website Has Been Built Using Next.js</text>
+				<p>The Aim of this Website is to showcase the programming skills of Brian Adams</p>
+				<p>There are several aspects of this webpage that one might like to know:</p>
+				<ol>
+					<li>It is being developed using Next.js, which is a React Framework that uses both static generation and server side generation to render different pages.</li>
+					<li>It is being written in TypeScript for the type checking that occurs at development time, instead of JavaScript&apos;s standard runtime type chekcing</li>
+				</ol>
 			</main>
 		</>
 	)
